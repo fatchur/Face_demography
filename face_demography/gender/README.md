340ae4c41c1f6a1131dbc0edd83fcb09e46ccf12
fatchur <fatchur.rahman1@gmail.com>
2019-03-02 11:54:40 +0700

    u

---
+++ face_demography/gender/README.md
@@ -0,0 +1,14 @@
+# FACE GENDER RECOGNITION WITH TENSORFLOW#
+--- UNDER DEVELOPMENT ---
+
+This project is a Convnet for recognizing face gender
+
+### Project Setup ###
+* install opencv 
+* install tensorflow
+* install matplotlib, numpy, sklearn
+or easily by "pip install -r requirements.txt"
+
+### Running The Project ###
+"python genRecog.py"
+
--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='simple_tensor',  # Required
-    version='v0.0.1',  # Required
-    description='a simple package for handling tensorflow tensor',  # Required
-    long_description="a simple package for handling tensorflow tensor",  # Optional
+    name='face_demography',  # Required
+    version='v0',  # Required
+    description='a collection of face gender, age, and expression model',  # Required
     author='Chur Chur',  # Optional
-    packages=["simple_tensor", "simple_tensor.transfer_learning", "simple_tensor.object_detector"],  # Required
+    packages=["face_demography"],  # Required
 )
